76f313cffe11b2d2ed177080de8ce6a1c7cf4923
Ameya <ameya.marakarkandy@gmail.com>
2023-07-04 20:40:53 +0530

    Added Google Maps
    added customized google maps using google maps API

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      mapId: "e32bcee3831de469",
+      center: { lat: 19.1334, lng: 72.9133 },
+      zoom: 15,
+    });
+  }
